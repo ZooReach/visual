39b4bd99131050295a6209671c09e8e01931a8e5
Abilash Rajasekaran <abilashr@IN-CBE-Abilash.local>
2019-04-11 17:06:35 +0530

    use dashboard template to display visual graph

--- app/helper/constants.py
+++ app/helper/constants.py
@@ -1,13 +1,12 @@
 environment_details = {
-    'ckan': 'http://139.59.66.4'
+    'ckan': 'http://68.183.89.90'
 }
 
 api = {
-    'datastore_search' : environment_details['ckan']+'/api/3/action/datastore_search',
-    'datastore_search_sql' : environment_details['ckan']+'/api/3/action/datastore_search_sql',
-
+    'datastore_search': environment_details['ckan'] + '/api/3/action/datastore_search',
+    'datastore_search_sql': environment_details['ckan'] + '/api/3/action/datastore_search_sql'
 }
 
 display_details = ['phylum', 'class', 'family', 'redlist_category']
 
-meta_data_resource_id="81ece0fb-2d6a-464f-9ea4-29a2aabf345a"
+meta_data_resource_id = "2804721b-d474-4f00-8249-49dc7d996d79"
